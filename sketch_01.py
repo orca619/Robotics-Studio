69ace10c1acdda841bce821142bdc599b6c3ffa4
orca619 <90537497+orca619@users.noreply.github.com>
2022-11-15 20:14:56 -0500

    Add files via upload

---
+++ sketch_01.py
@@ -0,0 +1,12 @@
+from lx16a import *
+
+LX16A.initialize("/dev/ttyUSB0", 0.1)
+
+try:
+    servo1 = LX16A(1)
+    servo1.set_angle_limits(0, 240)
+except ServoTimeoutError as e:
+    print(f"Servo {e.id_} is not responding. Exiting...")
+    quit()
+    
+servo1.move(120)
