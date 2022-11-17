824fd9b61d2632a7e04526064641fc6a7fe4c0e7
ibs2116 <ibs2116@gmail.com>
2022-11-16 21:55:25 -0500

    motorz biyotch

--- sketch_01.py
+++ sketch_01.py
@@ -4,10 +4,22 @@ import time
 LX16A.initialize("/dev/ttyUSB0", 0.1)
 
 try:
+    servo1 = LX16A(1)
     servo2 = LX16A(2)
     servo3 = LX16A(3)
+    servo1 = LX16A(4)
+    servo2 = LX16A(5)
+    servo3 = LX16A(6)
+    servo1 = LX16A(7)
+    servo2 = LX16A(8)
+    servo1.set_angle_limits(0, 240)
     servo2.set_angle_limits(0, 240)
     servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+    servo5.set_angle_limits(0, 240)
+    servo6.set_angle_limits(0, 240)
+    servo7.set_angle_limits(0, 240)
+    servo8.set_angle_limits(0, 240)
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
     quit()
@@ -15,23 +27,57 @@ except ServoTimeoutError as e:
 x = 0
 
 while True:
-    if x % 6 == 0: 
-        servo2.move(120)
-        time.sleep(1)
-    elif x % 6 == 1:
-        servo2.move(170)
-        time.sleep(1)
-    elif x % 6 == 2:
-        servo2.move(180)
-        time.sleep(1)
-    elif x % 6 == 3: 
-        servo3.move(120)
-        time.sleep(1)
-    elif x % 6 == 4:
-        servo3.move(170)
-        time.sleep(1)
-    elif x % 6 == 5:
-        servo3.move(180)
-        time.sleep(1)
+    servo1.move(60)
+    time.sleep(.5)
+    servo2.move(60)
+    time.sleep(.5)
+    servo3.move(60)
+    time.sleep(.5)
+    servo4.move(60)
+    time.sleep(.5)
+    servo5.move(60)
+    time.sleep(.5)
+    servo6.move(60)
+    time.sleep(.5)
+    servo7.move(60)
+    time.sleep(.5)
+    servo8.move(60)
+    time.sleep(.5)
+    servo1.move(120)
+    time.sleep(.5)
+    servo2.move(120)
+    time.sleep(.5)
+    servo3.move(120)
+    time.sleep(.5)
+    servo4.move(120)
+    time.sleep(.5)
+    servo5.move(120)
+    time.sleep(.5)
+    servo6.move(120)
+    time.sleep(.5)
+    servo7.move(120)
+    time.sleep(.5)
+    servo8.move(120)
+    time.sleep(.5)
+
+# while True:
+#     if x % 6 == 0: 
+#         servo2.move(120)
+#         time.sleep(1)
+#     elif x % 6 == 1:
+#         servo2.move(170)
+#         time.sleep(1)
+#     elif x % 6 == 2:
+#         servo2.move(180)
+#         time.sleep(1)
+#     elif x % 6 == 3: 
+#         servo3.move(120)
+#         time.sleep(1)
+#     elif x % 6 == 4:
+#         servo3.move(170)
+#         time.sleep(1)
+#     elif x % 6 == 5:
+#         servo3.move(180)
+#         time.sleep(1)
     
-    x += 1
+#     x += 1
