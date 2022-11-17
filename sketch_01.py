5b8bf0c991904ad907912de7cbfff1e1f3aa7745
ibs2116 <ibs2116@gmail.com>
2022-11-16 20:15:46 -0500

    2 motorzzzzzzzzzzzzzzz

--- sketch_01.py
+++ sketch_01.py
@@ -4,8 +4,10 @@ import time
 LX16A.initialize("/dev/ttyUSB0", 0.1)
 
 try:
-    servo1 = LX16A(1)
-    servo1.set_angle_limits(0, 240)
+    servo2 = LX16A(2)
+    servo3 = LX16A(3)
+    servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
     quit()
@@ -13,14 +15,23 @@ except ServoTimeoutError as e:
 x = 0
 
 while True:
-    if x % 3 == 0: 
-        servo1.move(120)
+    if x % 6 == 0: 
+        servo2.move(120)
         time.sleep(1)
-    elif x % 3 == 1:
-        servo1.move(170)
+    elif x % 6 == 1:
+        servo2.move(170)
         time.sleep(1)
-    elif x % 3 == 2:
-        servo1.move(180)
+    elif x % 6 == 2:
+        servo2.move(180)
+        time.sleep(1)
+    elif x % 6 == 3: 
+        servo3.move(120)
+        time.sleep(1)
+    elif x % 6 == 4:
+        servo3.move(170)
+        time.sleep(1)
+    elif x % 6 == 5:
+        servo3.move(180)
         time.sleep(1)
     
     x += 1
