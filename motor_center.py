6c530fd123de9334258a7cec2a184148c5736ac4
ibs2116 <ibs2116@gmail.com>
2022-11-18 12:03:06 -0500

    centering of motors

---
+++ motor_center.py
@@ -0,0 +1,34 @@
+from lx16a import *
+import time
+
+LX16A.initialize("/dev/ttyUSB0", 0.1)
+
+try:
+    servo1 = LX16A(1)
+    servo2 = LX16A(2)
+    servo3 = LX16A(3)
+    servo4 = LX16A(4)
+    servo5 = LX16A(5)
+    servo6 = LX16A(6)
+    servo7 = LX16A(7)
+    servo8 = LX16A(8)
+    servo1.set_angle_limits(0, 240)
+    servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+    servo5.set_angle_limits(0, 240)
+    servo6.set_angle_limits(0, 240)
+    servo7.set_angle_limits(0, 240)
+    servo8.set_angle_limits(0, 240)
+except ServoTimeoutError as e:
+    print(f"Servo {e.id_} is not responding. Exiting...")
+    quit()
+
+servo1.move(120)
+servo2.move(120)
+servo3.move(120)
+servo4.move(120)
+servo5.move(120)
+servo6.move(120)
+servo7.move(120)
+servo8.move(120)
