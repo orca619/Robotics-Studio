1ca1ee286b0f51ab7e5e12c2c3fe63c1f66df82a
ibs2116 <ibs2116@gmail.com>
2022-12-05 23:30:17 -0500

    nvorbvouwrv3borv

--- walk01.py
+++ walk01.py
@@ -27,8 +27,8 @@ except ServoTimeoutError as e:
 
 #angle = int(input("Enter desired motor angle for servo: "))
 #print(angle)
-angle = 60
-time.sleep(5)
+angle = 120
+time.sleep(4)
 
 atAngle1 = servo1.get_last_instant_move_hw()[0]
 print(atAngle1)
